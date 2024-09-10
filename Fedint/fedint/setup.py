1d981babc8dfde02f96d51b54e5c21d4025cadab
Amitash Nanda <amitashnanda01@gmail.com>
2024-09-09 22:27:18 -0700

    Update setup.py

--- Fedint/fedint/setup.py
+++ Fedint/fedint/setup.py
@@ -4,8 +4,8 @@ setup(
     name='fedint',
     version='0.1',
     description='Federated Learning Interpretability Tool',
-    author='Sree bhargavi balija',
-    author_email='sbalija@ucsd.edu',
+    author='Sree bhargavi balija', 'Amitash Nanda', 'Debashis Sahoo'
+    author_email='sbalija@ucsd.edu', 'ananda@ucsd.edu', 'dsahoo@ucsd.edu'
     packages=find_packages(),
     install_requires=[
         'torch>=1.8',
